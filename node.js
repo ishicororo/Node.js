525e7b4d96a424c166bf4f75a2a783b5c0b841b7
ishicororo <ishicororo.tks@gmail.com>
2025-06-21 08:24:52 +0000

    ejs

---
+++ node.js
@@ -0,0 +1,9 @@
+const fs=require('fs');
+const http=require('http');
+const ejs=require('ejs');
+const data=fs.readFileSync('indes.ejs','utf8')
+http.createServer(
+    (req,res){
+        res.
+    }
+)
